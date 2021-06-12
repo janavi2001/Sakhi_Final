b7e3aef5072ff7b33cb8a3c08898cbd02f49fffe
Manali <manalitanna29@gmail.com>
2021-06-12 11:28:23 +0530

    Personal Form UI

--- lib/Screens/PersonalForm/PersonalForm.dart
+++ lib/Screens/PersonalForm/PersonalForm.dart
@@ -25,261 +25,400 @@ class PersonalFormState extends State<PersonalForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildVillageName() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: ' Village Name'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Village Name is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: ' Village Name',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Village Name is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _VillageName = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _VillageName = value;
+        },
+      ),
     );
   }
 
   Widget _buildPatientName() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: ' Patient Name'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Patient Name is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: ' Patient Name',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Patient Name is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _PatientName = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _PatientName = value;
+        },
+      ),
     );
   }
 
   Widget _buildAge() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Age'),
-      keyboardType: TextInputType.number,
-      validator: (String value) {
-        int Age = int.tryParse(value);
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Age',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        keyboardType: TextInputType.number,
+        validator: (String value) {
+          int Age = int.tryParse(value);
 
-        if (Age == null || Age <= 0) {
-          return 'Age must be greater than 0';
-        }
+          if (Age == null || Age <= 0) {
+            return 'Age must be greater than 0';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _Age = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _Age = value;
+        },
+      ),
     );
   }
 
   Widget _buildEducation() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Education'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Education is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Education',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Education is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _Education = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _Education = value;
+        },
+      ),
     );
   }
 
   Widget _buildCaste() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Caste'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Caste is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Caste',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Caste is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _Caste = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _Caste = value;
+        },
+      ),
     );
   }
 
   Widget _buildFamilyMembers() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Family Members'),
-      keyboardType: TextInputType.number,
-      validator: (String value) {
-        int FamilyMembers = int.tryParse(value);
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Family Members',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        keyboardType: TextInputType.number,
+        validator: (String value) {
+          int FamilyMembers = int.tryParse(value);
 
-        if (FamilyMembers == null) {
-          return 'Enter a Valid number';
-        }
+          if (FamilyMembers == null) {
+            return 'Enter a Valid number';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _FamilyMembers = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _FamilyMembers = value;
+        },
+      ),
     );
   }
 
   Widget _buildGender() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Gender'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Gender is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Gender',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Gender is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _Gender = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _Gender = value;
+        },
+      ),
     );
   }
 
   Widget _buildProfession() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Profession'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Profession is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Profession',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Profession is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _Profession = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _Profession = value;
+        },
+      ),
     );
   }
 
   Widget _buildWorkingClass() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Working Class'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Working Class is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Working Class',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Working Class is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _WorkingClass = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _WorkingClass = value;
+        },
+      ),
     );
   }
 
   Widget _buildMarriageStatus() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Marriage Status'),
-      maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Marriage Status is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Marriage Status',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Marriage Status is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _MarriageStatus = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _MarriageStatus = value;
+        },
+      ),
     );
   }
 
   Widget _buildPhoneNumber() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Phone number'),
-      keyboardType: TextInputType.phone,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Phone number is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Phone number',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        keyboardType: TextInputType.phone,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Phone number is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _PhoneNumber = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _PhoneNumber = value;
+        },
+      ),
     );
   }
 
   Widget _buildAddress() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Address'),
-      maxLength: 30,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Address is Required';
-        }
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        keyboardType: TextInputType.multiline,
+        decoration: InputDecoration(
+          labelText: 'Address',
+          labelStyle: TextStyle(color: textColor),
+          border: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(10.0),
+            ),
+          ),
+        ),
+        maxLength: 30,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Address is Required';
+          }
 
-        return null;
-      },
-      onSaved: (String value) {
-        _Address = value;
-      },
+          return null;
+        },
+        onSaved: (String value) {
+          _Address = value;
+        },
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Personal Details of Patient')),
+      appBar: AppBar(
+        title: Text(
+          'Personal Details of Patient',
+          style: TextStyle(color: headingColor),
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(24),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _buildVillageName(),
-                _buildPatientName(),
-                _buildAge(),
-                _buildEducation(),
-                _buildCaste(),
-                _buildFamilyMembers(),
-                _buildGender(),
-                _buildProfession(),
-                _buildWorkingClass(),
-                _buildMarriageStatus(),
-                _buildPhoneNumber(),
-                _buildAddress(),
-                SizedBox(height: 100),
-                ElevatedButton(
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(color: textColor, fontSize: 18),
+        child: Theme(
+          data: ThemeData(
+            primaryColor: yellowTheme,
+            primaryColorDark: textColor,
+            accentColor: yellowTheme,
+          ),
+          child: Container(
+            margin: EdgeInsets.all(24),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  _buildPatientName(),
+                  _buildAge(),
+                  _buildGender(),
+                  _buildPhoneNumber(),
+                  _buildVillageName(),
+                  _buildAddress(),
+                  _buildCaste(),
+                  _buildFamilyMembers(),
+                  _buildEducation(),
+                  _buildProfession(),
+                  _buildWorkingClass(),
+                  _buildMarriageStatus(),
+                  SizedBox(height: 100),
+                  ElevatedButton(
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(color: textColor, fontSize: 18),
+                    ),
+                    onPressed: () {
+                      if (!_formKey.currentState.validate()) {
+                        return;
+                      }
+
+                      _formKey.currentState.save();
+                    },
                   ),
-                  onPressed: () {
-                    if (!_formKey.currentState.validate()) {
-                      return;
-                    }
-
-                    _formKey.currentState.save();
-
-                    /*print(_name);
-                    print(_email);
-                    print(_phoneNumber);
-                    print(_url);
-                    print(_password);
-                    print(_calories);*/
-
-                    //Send to API
-                  },
-                )
-              ],
+                  SizedBox(
+                    height: 50,
+                  )
+                ],
+              ),
             ),
           ),
         ),
